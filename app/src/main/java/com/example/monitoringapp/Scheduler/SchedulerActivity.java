6295564476bd0d5b61633aac065ce980eb9de2ac
unknown <sanghee0610@ajou.ac.kr>
2021-01-14 13:14:57 +0900

    Update date selection

--- app/src/main/java/com/example/monitoringapp/Scheduler/SchedulerActivity.java
+++ app/src/main/java/com/example/monitoringapp/Scheduler/SchedulerActivity.java
@@ -10,10 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.monitoringapp.R;
 import com.example.monitoringapp.databinding.ActivitySchedulerBinding;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -38,8 +40,10 @@ public class SchedulerActivity extends AppCompatActivity {
     public static String sc_EXETM; // 예상실행시간
     public static String sc_UPDDT; // 실제실행일자
     public static String sc_UPDTM; // 실제실행시간
-    private String STARTDT;
-    private String ENDDT;
+    private String STARTDT = null;
+    private String ENDDT = null;
+
+    private int mStartYear, mStartMonth, mStartDay, mEndYear, mEndMonth, mEndDay;
 
     public static ArrayList<SchedulerItem> schedulerList = new ArrayList<>(); // 스케줄러 목록
 
@@ -63,9 +67,36 @@ public class SchedulerActivity extends AppCompatActivity {
         // 초기 날짜 (미리 지정되어있음)
         long now = System.currentTimeMillis();
         Date mDate = new Date(now);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String init_dt = simpleDateFormat.format(mDate);
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("y");
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("MM");
+        SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("dd");
+
+        int init_year = Integer.parseInt(simpleDateFormat1.format(mDate));
+        int init_month = Integer.parseInt(simpleDateFormat2.format(mDate));
+        int init_day = Integer.parseInt(simpleDateFormat3.format(mDate));
+
+        System.out.println("년도 : " + init_year + " 월 : " + init_month + " 일 : " + init_day);
+
+        String st_init_month = null;
+        String st_init_day = null;
+
+        if (init_month < 10) {
+            st_init_month = "0" + Integer.toString(init_month);
+        } else {
+            st_init_month = Integer.toString(init_month);
+        }
+        if (init_day < 10) {
+            st_init_day = "0" + Integer.toString(init_day);
+        }
+        else {
+            st_init_day = Integer.toString(init_day);
+        }
+
+        final String init_dt = Integer.toString(init_year) + "-" + st_init_month + "-" + st_init_day;
+
         tv_start.setText(init_dt);
+        STARTDT = init_dt;
+        ENDDT = init_dt;
         tv_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,25 +110,32 @@ public class SchedulerActivity extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                         // i : year, i1 : month, i2 : day
+
+                        mStartYear = i;
+                        mStartMonth = i1 + 1;
+                        mStartDay = i2;
+
                         // i1에서 1월이면 0, 2월이면 1, 3월이면 2가 나오므로 1을 더해줘야함
-                        int forMonth = i1 + 1;
                         String startdt;
-                        System.out.println("i : " + i + "  i1 : " + forMonth + "  i2 : " + i2);
-                        if (forMonth < 10) {
-                            startdt = i + "-0" + forMonth + "-";
+                        System.out.println("i : " + mStartYear + "  i1 : " + mStartMonth + "  i2 : " + mStartDay);
+
+                        // 날짜를 2021-01-01 형식으로 맞춰주기
+                        if (mStartMonth < 10) {
+                            startdt = i + "-0" + mStartMonth + "-";
                         } else {
-//                            String tmp = Integer.toString(forMonth);
-                            startdt = i + "-" + forMonth + "-";
+                            startdt = i + "-" + mStartMonth + "-";
                         }
 
                         if (i2 < 10) {
-                            startdt = startdt + "0" + i2;
+                            startdt = startdt + "0" + mStartDay;
                         }
                         else {
-                            startdt = startdt + i2;
+                            startdt = startdt + mStartDay;
                         }
-                        System.out.println(startdt);
+                        System.out.println("선택된 시작 날짜 : " + startdt);
                         tv_start.setText(startdt);
+
+                        STARTDT = startdt;
                     }
                 }, mYear, mMonth, mDay);
                 mDatePickerDialog.show();
@@ -120,13 +158,17 @@ public class SchedulerActivity extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                         // i : year, i1 : month, i2 : day
-                        int forMonth = i1 + 1;
+
+                        mEndYear = i;
+                        mEndMonth = i1 + 1;
+                        mEndDay = i2;
+
                         String enddt;
-                        System.out.println("i : " + i + "  i1 : " + forMonth + "  i2 : " + i2);
-                        if (forMonth < 10) {
-                            enddt = i + "-0" + forMonth + "-";
+                        System.out.println("i : " + i + "  i1 : " + mEndMonth + "  i2 : " + i2);
+                        if (mEndMonth < 10) {
+                            enddt = i + "-0" + mEndMonth + "-";
                         } else {
-                            enddt = i + "-" + forMonth + "-";
+                            enddt = i + "-" + mEndMonth + "-";
                         }
 
                         if (i2 < 10) {
@@ -135,19 +177,66 @@ public class SchedulerActivity extends AppCompatActivity {
                         else {
                             enddt = enddt + i2;
                         }
-                        System.out.println(enddt);
+                        System.out.println("선택된 끝 날짜 : " + enddt);
                         tv_end.setText(enddt);
+
+                        ENDDT = enddt;
                     }
                 }, mYear, mMonth, mDay);
                 mDatePickerDialog.show();
             }
         });
 
+//        // init_dt 에서 parse 해오기
+//        final int parseYear, parseMonth, parseDay;
+//        System.out.println("init_dt : " + init_dt);
+//
+//        parseYear = Integer.parseInt(init_dt.substring(0, 4));
+//        System.out.println("init_dt : " + parseYear);
+//        parseMonth = Integer.parseInt(init_dt.substring(4, 6));
+//        System.out.println("init_dt : " + parseMonth);
+//        parseDay = Integer.parseInt(init_dt.substring(6, 8));
+//        System.out.println("init_dt : " + parseDay);
+
         btn_day = binding.schedulerBtnDay;
+        btn_day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_start.setText(init_dt);
+                tv_end.setText(init_dt);
+
+                STARTDT = init_dt;
+                ENDDT = init_dt;
+            }
+        });
 
         btn_week = binding.schedulerBtnWeek;
+        btn_week.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_start.setText(init_dt);
+                STARTDT = init_dt;
+                System.out.println("tv_start" + tv_start.getText().toString());
+                String enddt;
+                enddt = getLaterDay(tv_start.getText().toString(), 7);
+                tv_end.setText(enddt);
+                ENDDT = enddt;
+            }
+        });
 
         btn_month = binding.schedulerBtnDateMonth;
+        btn_month.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_start.setText(init_dt);
+                STARTDT = init_dt;
+                System.out.println("tv_start" + tv_start.getText().toString());
+                String enddt;
+                enddt = getLaterMonth(tv_start.getText().toString(), 1);
+                tv_end.setText(enddt);
+                ENDDT = enddt;
+            }
+        });
 
         btn_searchScheduler = binding.schedulerBtnSearch;
         btn_searchScheduler.setOnClickListener(new View.OnClickListener() {
@@ -178,6 +267,8 @@ public class SchedulerActivity extends AppCompatActivity {
                     hideBtnClicked = true;
 //                    spinner.setVisibility(view.GONE);
                     tv_scheduler_label.setVisibility(view.GONE);
+                    tv_start.setVisibility(view.GONE);
+                    tv_end.setVisibility(view.GONE);
                     tv_divider.setVisibility(view.GONE);
                     btn_searchScheduler.setVisibility(view.GONE);
                     btn_day.setVisibility(view.GONE);
@@ -187,6 +278,8 @@ public class SchedulerActivity extends AppCompatActivity {
                     hideBtnClicked = false;
 //                    spinner.setVisibility(view.VISIBLE);
                     tv_scheduler_label.setVisibility(view.VISIBLE);
+                    tv_start.setVisibility(view.VISIBLE);
+                    tv_end.setVisibility(view.VISIBLE);
                     tv_divider.setVisibility(view.VISIBLE);
                     btn_searchScheduler.setVisibility(view.VISIBLE);
                     btn_day.setVisibility(view.VISIBLE);
@@ -195,5 +288,87 @@ public class SchedulerActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private String getLaterDay(String originDay, int laterCnt) {
+        System.out.println("originday" + originDay);
+        String laterDay = null;
+
+        try {
+            Calendar calendar = Calendar.getInstance();
+            Date originDate = new Date();
+            Date laterDate = new Date();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD");
+//            SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyyMMdd");
+//
+//            System.out.println("test");
+//            System.out.println(originDay); // 선택시작날짜
+            originDate = simpleDateFormat.parse(originDay);
+            calendar.setTime(laterDate);
+//
+//            System.out.println(originDate); // 이상한값이나와
+
+            calendar.add(Calendar.DATE, laterCnt); // laterCnt일 후의 날짜를 구한다.
+            laterDate = calendar.getTime();
+            laterDay = simpleDateFormat.format(laterDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("laterDay" + laterDay);
+        return laterDay;
+    }
+
+    private String getLaterMonth(String originDay, int laterCnt) {
+        System.out.println("originday" + originDay);
+        String originDay2 = originDay.replaceAll("-" ,"");
+
+        String tmp_year = originDay.substring(0, 4);
+        String tmp_month = originDay.substring(5, 7);
+        String tmp_day =  originDay.substring(8, 10);
+
+        System.out.println("tmp_year : " + tmp_year + " tmp_month: " + tmp_month + "  tmp_day : " + tmp_day);
+        String tmp_date = null;
+
+        String laterDay = null;
+
+        try {
+            Calendar calendar = Calendar.getInstance();
+            Date originDate = new Date();
+            Date laterDate = new Date();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+            System.out.println("test");
+            System.out.println(originDay); // 선택시작날짜
+            originDate = simpleDateFormat.parse(originDay);
+            calendar.setTime(laterDate);
+//
+//            System.out.println(originDate); // 이상한값이나와
+
+            calendar.add(Calendar.MONTH, laterCnt); // laterCnt 개월 후의 월을 구한다.
+            laterDate = calendar.getTime();
+            laterDay = simpleDateFormat.format(laterDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        tmp_year = laterDay.substring(0, 4);
+        tmp_month = laterDay.substring(4, 6);
+        tmp_day =  laterDay.substring(6, 8);
+
+        System.out.println("tmp_year : " + tmp_year + " tmp_month: " + tmp_month + "  tmp_day : " + tmp_day);
+
+        // 날짜를 2021-01-01 형식으로 맞춰주기
+        tmp_date = tmp_year + "-" + tmp_month + "-";
+
+        if (Integer.parseInt(tmp_day) < 10) {
+            tmp_date = tmp_date + "0" + tmp_day;
+        }
+        else {
+            tmp_date = tmp_date + tmp_day;
+        }
+
+        System.out.println("laterMonth" + laterDay);
+        return tmp_date;
     }
 }
